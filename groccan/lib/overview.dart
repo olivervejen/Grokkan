e75878866c3018db732e285f32f28c25b8382ff6
Oliver Vejen <oliver@dare.solutions>
2021-07-06 18:32:31 +0200

    Font integration and folder structure

---
+++ groccan/lib/overview.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class Overview extends StatefulWidget {
+  Overview({Key? key}) : super(key: key);
+  final OverviewState state = OverviewState();
+  @override
+  State<Overview> createState() => OverviewState();
+}
+
+class OverviewState extends State<Overview> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Expanded(
+            child: Container(
+          decoration: const BoxDecoration(
+            color: Colors.red,
+          ),
+          child: const Center(child: Text("buuh")),
+        )),
+        Positioned(
+            child: Container(
+                width: 300,
+                height: 300,
+                decoration: const BoxDecoration(color: Colors.green),
+                child: const Text("YAAAA")))
+      ],
+    );
+  }
+}
