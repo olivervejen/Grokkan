8da600d7a8e2be7c6ab6049d8a355e2827791b10
Oliver <olivervejen47@gmail.com>
2022-10-01 21:24:52 +0200

    New version of Flutter

---
+++ grokkan_Old/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:grokkan/overview.dart';
+
+void main() {
+  runApp(const Grokkan());
+}
+
+class Grokkan extends StatelessWidget {
+  const Grokkan({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue, fontFamily: "SpaceGrotesk"),
+      home: Scaffold(
+        body: Overview(),
+      ),
+    );
+  }
+}
